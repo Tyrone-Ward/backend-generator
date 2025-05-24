d8466ce0802c5a98639ca19fadf5c824fa528a9f
Tyrone Ward <gameplace123@gmail.com>
2025-05-24 00:41:48 -0400

    feat: db question

--- index.js
+++ index.js
@@ -5,22 +5,62 @@ import path from 'path'
 import { exec } from 'child_process'
 import util from 'util'
 import { argv } from 'node:process'
-import { select } from '@inquirer/prompts'
+import { select, Separator } from '@inquirer/prompts'
 
 const execPromise = util.promisify(exec)
 
-const answer2 = await select({
+const authenticationAnswer = await select({
   message: 'Do you need authentication?',
   choices: [
     {
       name: 'Yes',
       value: true,
-      description: 'Adds Clerk authentication and user management to your React application.'
+      description: 'Good for an API'
     },
     {
       name: 'No',
       value: false,
-      description: 'Roll your own authentication service.'
+      description: 'Suitable for tooling.'
+    }
+  ]
+})
+
+let authenticationDetails
+
+if (authenticationAnswer) {
+  authenticationDetails = await select({
+    message: 'Auth type',
+    choices: [
+      {
+        name: 'Clerk',
+        value: '@clerk/backend',
+        description: 'Adds Clerk authentication and user management to your project.'
+      },
+      {
+        name: 'JSON Web Token',
+        value: 'jsonwebtoken', // jsonwebtoken
+        description: 'JWT Authentication.'
+      }
+    ]
+  })
+}
+
+const database = await select({
+  message: 'Select a database.',
+  choices: [
+    {
+      name: 'SQLite3',
+      value: 'sqlite3',
+      description: 'Install asynchronous, non-blocking SQLite3 bindings for Node.js.'
+    },
+    {
+      name: 'MongoDB',
+      value: 'mongodb',
+      description: 'Install official MongoDB driver for Node.js'
+    },
+    {
+      name: 'No database',
+      value: undefined
     }
   ]
 })
@@ -43,8 +83,8 @@ function cloneRepository(repoUrl) {
     console.log(`Repository cloned.`)
   })
 }
-
-function updatePackageJson(folderName, folderPath) {
+// TDOD: add dependencies based on user input
+function updatePackageJson(folderName, folderPath, options) {
   const packageJsonPath = path.join(folderPath, 'package.json')
 
   return fs
@@ -59,6 +99,9 @@ function updatePackageJson(folderName, folderPath) {
     })
 }
 
+const options = { authentication: authenticationDetails, database }
+console.log(options)
+
 const folderName = argv[2]
 const repoUrl = 'https://github.com/Tyrone-Ward/nodejs-template.git'
 
@@ -67,7 +110,8 @@ async function cloneRepo(folderName, repoUrl) {
     const folderPath = await createFolder(folderName)
     await changeWorkingDirectory(folderPath)
     await cloneRepository(repoUrl)
-    await updatePackageJson(folderName, folderPath)
+    await updatePackageJson(folderName, folderPath, options)
+    console.log(options)
     console.log('Run "npm i" to install packages.')
   } catch (error) {
     console.error('Error:', error.message)
